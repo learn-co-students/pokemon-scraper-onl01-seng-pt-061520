8db3cf78a75e48ebb44de2002c99ebbb0a93b243
github username <charliepbarth@gmail.com>
2020-08-19 17:56:20 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
       @id, @name, @type, @db = id, name, type, db
     end
 
-    def self.save(name,type,db)
+    def self.save(name, type, db)
       db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)",name,type)
     end
 
