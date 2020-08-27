c953b778c4fa6083cce76e8627ae02eea01b4e91
drewsview34 <drewsview34@outlook.com>
2020-08-26 20:57:27 -0700

    Created the initialize method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+
+    attr_accessor :name, :type
+    attr_reader :id, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        
 end
