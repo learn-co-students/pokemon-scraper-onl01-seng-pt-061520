76f702c4c1775c5b5a2e732f817c7fbd37b93933
drewsview34 <drewsview34@outlook.com>
2020-08-26 21:03:51 -0700

    Created the save Class method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,5 +11,15 @@ class Pokemon
     end
 
     def self.save(name, type, db)
-        
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find
+
+    end
 end
